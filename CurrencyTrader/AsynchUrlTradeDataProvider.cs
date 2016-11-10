23383c4159e8ba24506e9c890e880f93801350d9
Tracy Yang <jewel7meme@gmail.com>
2016-11-10 11:02:32 -0600

    Partially implement asynch command

---
+++ CurrencyTrader/AsynchUrlTradeDataProvider.cs
@@ -0,0 +1,29 @@
+﻿using CurrencyTrader;
+using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrader
+{
+    public class AsynchUrlTradeDataProvider : ITradeDataProvider
+
+    {
+        private readonly String url;
+        UrlTradeDataProvider SynchTradeProvider;
+        public AsynchUrlTradeDataProvider(string url)
+        {
+            this.url = url;
+            SynchTradeProvider = new UrlTradeDataProvider(url);
+        }
+        public IEnumerable<string> GetTradeData()
+        {
+            //return SynchTradeProvider.GetTradeData();
+            Task.Run(() => SynchTradeProvider.GetTradeData());
+        }
+
+        
+    }
+}
